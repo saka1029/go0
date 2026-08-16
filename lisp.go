7788b22f7b62710b160db0944d66d8e78adafd54
saka1029 <saka1029@gmail.com>
2026-08-16 12:49:54 +0900

    ..

--- lisp.go
+++ lisp.go
@@ -2,63 +2,101 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	// "strconv"
 )
 
 type Context struct {
-	globals map[Symbol]Evaluable
+	globals map[string]Evaluable
 }
 
 func context() *Context {
-	return &Context{map[Symbol]Evaluable{}}
+	return &Context{map[string]Evaluable{}}
 }
 
 type Evaluable interface {
-	eval(context *Context) Evaluable
-	apply(args Evaluable, context *Context) Evaluable
-	toString() string
+// 	// eval(context *Context) Evaluable
+// 	// apply(args Evaluable, context *Context) Evaluable
+// 	// toString() string
 }
 
-type Symbol string
-func (this Symbol)eval(context *Context) Evaluable {
-	return context.globals[this]
-}
-func (this Symbol)apply(args Evaluable, context *Context) Evaluable {
-	panic("can't apply")
-}
-func (this Symbol)toString() string {
-	return string(this)
-}
+// type Symbol string
+// func (this Symbol)eval(context *Context) Evaluable {
+// 	return context.globals[this]
+// }
+// func (this Symbol)apply(args Evaluable, context *Context) Evaluable {
+// 	panic("can't apply")
+// }
+// func (this Symbol)toString() string {
+// 	return string(this)
+// }
 
-type Integer int
-func (this Integer)eval(context *Context) Evaluable {
-	return this
-}
-func (this Integer)apply(args Evaluable, context *Context) Evaluable {
-	panic("can't apply");
-}
-func (this Integer)toString() string {
-	return strconv.Itoa(int(this))
-}
+// type Integer int
+// func (this Integer)eval(context *Context) Evaluable {
+// 	return this
+// }
+// func (this Integer)apply(args Evaluable, context *Context) Evaluable {
+// 	panic("can't apply");
+// }
+// func (this Integer)toString() string {
+// 	return strconv.Itoa(int(this))
+// }
 
 type Cons struct {
 	car, cdr Evaluable
 }
-func (this Cons)eval(context *Context) Evaluable {
-	return this.car.eval(context).apply(this.cdr, context)
+// func (this Cons)eval(context *Context) Evaluable {
+// 	return this.car.eval(context).apply(this.cdr, context)
+// }
+// func (this Cons)apply(args Evaluable, context *Context) Evaluable {
+// 	panic("can't apply");
+// }
+// func (this Cons)toString() string {
+// 	return fmt.Sprintf("(%s . %s)", this.car.toString(), this.cdr.toString())
+// }
+
+func check(v interface{}) {
+	switch x := v.(type) {
+	case int:
+		fmt.Println("int ", x)
+	// case Integer:
+	// 	fmt.Println("Integer %d\n", x)
+	case string:
+		fmt.Println("string ", x)
+	default:
+		fmt.Println("unknown ", x)
+	}
 }
-func (this Cons)apply(args Evaluable, context *Context) Evaluable {
-	panic("can't apply");
+
+func eval(e Evaluable, c *Context) Evaluable {
+	switch v := e.(type) {
+	case int:
+		return v
+	case string:
+		return c.globals[v]
+	case Cons:
+		return apply(eval(v.car, c), v.cdr, c)
+	default:
+		panic(fmt.Sprintf("unknown type %T", v))
+	}
 }
-func (this Cons)toString() string {
-	return fmt.Sprintf("(%s . %s)", this.car.toString(), this.cdr.toString())
+
+func apply(e Evaluable, args Evaluable, c *Context) Evaluable {
+	return nil
 }
 
 func main() {
 	var context *Context = context()
-	context.globals[Symbol("symbol")] = Symbol("value")
-	fmt.Println(Symbol("symbol").eval(context))
-	fmt.Println(Integer(123).eval(context))
-	fmt.Println(Cons{Integer(345), Symbol("abc")}.toString())
+	fmt.Println(context)
+	context.globals["symbol"] = "value"
+	fmt.Println(eval("symbol", context))
+	fmt.Println(eval(123, context))
+	// fmt.Println(Symbol("symbol").eval(context))
+	// fmt.Println(Integer(123).eval(context))
+	// fmt.Println(Cons{Integer(345), Symbol("abc")}.toString())
+	// check(123)
+	// check("abc")
+	// check(Cons{2,3})
+	// check(Integer(123))
+	// check(Symbol("sym"))
 }
 
