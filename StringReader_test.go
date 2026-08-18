2a8df68f02fec96f6eec1be2cb9bfb171b7eb66f
saka1029 <saka1029@gmail.com>
2026-08-18 20:54:52 +0900

    ..

--- StringReader_test.go
+++ StringReader_test.go
@@ -1,34 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
-func stringReaderTest(t *testing.T, s string, expected int) {
-	actual := stringLength(s[0])
-	fmt.Printf("%s length=%d\n", s, actual)
-	if expected != actual {
-		t.Errorf("TestString %s -> %d not %d", s, actual, expected)
-	}
-}
-func stringReaderTest2(t *testing.T, sr *StringReader, expected string) {
-	actual := sr.Get()
-	fmt.Printf("%s expected=%s\n", actual, expected)
-	if expected != actual {
-		t.Errorf("TestString %s not %s", actual, expected)
-	}
-}
-
 func TestStringReader(t *testing.T) {
-	stringReaderTest(t, "a", 1)
-	stringReaderTest(t, "ä", 2)
-	stringReaderTest(t, "あ", 3)
-	stringReaderTest(t, "𩸽", 4)
+	m := "TestStringReader"
+	assertEquals(t, m, "a", 1, stringLength("a"[0]))
+	assertEquals(t, m, "ä", 2, stringLength("ä"[0]))
+	assertEquals(t, m, "あ", 3, stringLength("あ"[0]))
+	assertEquals(t, m, "𩸽", 4, stringLength("𩸽"[0]))
 	sr := NewStringReader("aäあ𩸽")
-	stringReaderTest2(t, sr, "a")
-	stringReaderTest2(t, sr, "ä")
-	stringReaderTest2(t, sr, "あ")
-	stringReaderTest2(t, sr, "𩸽")
-	stringReaderTest2(t, sr, "")
+	assertEquals(t, m, "a", "a", sr.Get())
+	assertEquals(t, m, "ä", "ä", sr.Get())
+	assertEquals(t, m, "あ", "あ", sr.Get())
+	assertEquals(t, m, "𩸽", "𩸽", sr.Get())
+	assertEquals(t, m, "", "", sr.Get())
 }
--- lisp_test.go
+++ lisp_test.go
@@ -19,57 +19,52 @@ func quote(e Evaluable) Evaluable {
 }
 
 func TestEval(t *testing.T) {
+	m := "TestEval"
 	e := env()
-	assertEquals(t, "TestEval", "abc", "abc", eval("abc", e))
-	assertEquals(t, "TestEval", "123", 123, eval(123, e))
-	assertEquals(t, "TestEval", "true", true, eval(true, e))
-	assertEquals(t, "TestEval", "'a", sym("a"), eval(quote(sym("a")), e).(Symbol))
-	assertEquals(t, "TestEval", "'(1 2 3 4)", list(1, 2, 3, 4), eval(quote(list(1, 2, 3, 4)), e))
-	assertEquals(t, "TestEval", "(car '(1 2 3 4))", 1, eval(list(sym("car"), quote(list(1, 2, 3, 4))), e))
-	assertEquals(t, "TestEval", "(cdr '(1 2 3 4))", list(2, 3, 4), eval(list(sym("cdr"), quote(list(1, 2, 3, 4))), e))
-	assertEquals(t, "TestEval", "(cons 'a '(1 2 3 4))", list(sym("a"), 1, 2, 3, 4), eval(list(sym("cons"), quote(sym("a")), quote(list(1, 2, 3, 4))), e))
-	assertEquals(t, "TestEval", "(cons 'a 'b)", cons(sym("a"), sym("b")), eval(list(sym("cons"), quote(sym("a")), quote(sym("b"))), e).(Cons))
+	assertEquals(t, m, "abc", "abc", eval("abc", e))
+	assertEquals(t, m, "123", 123, eval(123, e))
+	assertEquals(t, m, "true", true, eval(true, e))
+	assertEquals(t, m, "'a", sym("a"), eval(quote(sym("a")), e).(Symbol))
+	assertEquals(t, m, "'(1 2 3 4)", list(1, 2, 3, 4), eval(quote(list(1, 2, 3, 4)), e))
+	assertEquals(t, m, "(car '(1 2 3 4))", 1, eval(list(sym("car"), quote(list(1, 2, 3, 4))), e))
+	assertEquals(t, m, "(cdr '(1 2 3 4))", list(2, 3, 4), eval(list(sym("cdr"), quote(list(1, 2, 3, 4))), e))
+	assertEquals(t, m, "(cons 'a '(1 2 3 4))", list(sym("a"), 1, 2, 3, 4), eval(list(sym("cons"), quote(sym("a")), quote(list(1, 2, 3, 4))), e))
+	assertEquals(t, m, "(cons 'a 'b)", cons(sym("a"), sym("b")), eval(list(sym("cons"), quote(sym("a")), quote(sym("b"))), e).(Cons))
 }
 
 func TestArithmetic(t *testing.T) {
+	m := "TestArithmetic"
 	e := env()
-	assertEquals(t, "TestArithmetic", "(+)", 0, eval(list(sym("+")), e))
-	assertEquals(t, "TestArithmetic", "(+ 1)", 1, eval(list(sym("+"), 1), e))
-	assertEquals(t, "TestArithmetic", "(+ 1 2)", 3, eval(list(sym("+"), 1, 2), e))
-	assertEquals(t, "TestArithmetic", "(+ 1 2 3 4)", 10, eval(list(sym("+"), 1, 2, 3, 4), e))
-	assertEquals(t, "TestArithmetic", "(+ 1 2 (+ 3 4) 5)", 15, eval(list(sym("+"), 1, 2, list(sym("+"), 3, 4), 5), e))
-	assertEquals(t, "TestArithmetic", "(-)", 0, eval(list(sym("-")), e))
-	assertEquals(t, "TestArithmetic", "(- 1)", -1, eval(list(sym("-"), 1), e))
-	assertEquals(t, "TestArithmetic", "(- 3 2)", 1, eval(list(sym("-"), 3, 2), e))
-	assertEquals(t, "TestArithmetic", "(- 1 2 3 4)", -8, eval(list(sym("-"), 1, 2, 3, 4), e))
-	assertEquals(t, "TestArithmetic", "(*)", 1, eval(list(sym("*")), e))
-	assertEquals(t, "TestArithmetic", "(* 3)", 3, eval(list(sym("*"), 3), e))
-	assertEquals(t, "TestArithmetic", "(* 2 3)", 6, eval(list(sym("*"), 2, 3), e))
-	assertEquals(t, "TestArithmetic", "(* 1 2 3 4)", 24, eval(list(sym("*"), 1, 2, 3, 4), e))
-	assertEquals(t, "TestArithmetic", "(* 1 2 (+ 3 4) 5)", 70, eval(list(sym("*"), 1, 2, list(sym("+"), 3, 4), 5), e))
-	assertEquals(t, "TestArithmetic", "(/)", 1, eval(list(sym("/")), e))
-	assertEquals(t, "TestArithmetic", "(/ 10)", 0, eval(list(sym("/"), 10), e))
-	assertEquals(t, "TestArithmetic", "(/ 3 2)", 1, eval(list(sym("/"), 3, 2), e))
-	assertEquals(t, "TestArithmetic", "(/ 1001 7 11)", 13, eval(list(sym("/"), 1001, 7, 11), e))
-}
-
-func printTest(t *testing.T, expected string, e Evaluable) {
-	actual := print(e)
-	fmt.Println("TestPrint:", actual)
-	if actual != expected {
-		t.Errorf("print %s not %s", actual, expected)
-	}
+	assertEquals(t, m, "(+)", 0, eval(list(sym("+")), e))
+	assertEquals(t, m, "(+ 1)", 1, eval(list(sym("+"), 1), e))
+	assertEquals(t, m, "(+ 1 2)", 3, eval(list(sym("+"), 1, 2), e))
+	assertEquals(t, m, "(+ 1 2 3 4)", 10, eval(list(sym("+"), 1, 2, 3, 4), e))
+	assertEquals(t, m, "(+ 1 2 (+ 3 4) 5)", 15, eval(list(sym("+"), 1, 2, list(sym("+"), 3, 4), 5), e))
+	assertEquals(t, m, "(-)", 0, eval(list(sym("-")), e))
+	assertEquals(t, m, "(- 1)", -1, eval(list(sym("-"), 1), e))
+	assertEquals(t, m, "(- 3 2)", 1, eval(list(sym("-"), 3, 2), e))
+	assertEquals(t, m, "(- 1 2 3 4)", -8, eval(list(sym("-"), 1, 2, 3, 4), e))
+	assertEquals(t, m, "(*)", 1, eval(list(sym("*")), e))
+	assertEquals(t, m, "(* 3)", 3, eval(list(sym("*"), 3), e))
+	assertEquals(t, m, "(* 2 3)", 6, eval(list(sym("*"), 2, 3), e))
+	assertEquals(t, m, "(* 1 2 3 4)", 24, eval(list(sym("*"), 1, 2, 3, 4), e))
+	assertEquals(t, m, "(* 1 2 (+ 3 4) 5)", 70, eval(list(sym("*"), 1, 2, list(sym("+"), 3, 4), 5), e))
+	assertEquals(t, m, "(/)", 1, eval(list(sym("/")), e))
+	assertEquals(t, m, "(/ 10)", 0, eval(list(sym("/"), 10), e))
+	assertEquals(t, m, "(/ 3 2)", 1, eval(list(sym("/"), 3, 2), e))
+	assertEquals(t, m, "(/ 1001 7 11)", 13, eval(list(sym("/"), 1001, 7, 11), e))
 }
 
 func TestPrint(t *testing.T) {
-	printTest(t, "abc", sym("abc"))
-	printTest(t, "123", 123)
-	printTest(t, "true", true)
-	printTest(t, "()", list())
-	printTest(t, "(1 a)", list(1, sym("a")))
-	printTest(t, "'(1 a)", quote(list(1, sym("a"))))
-	printTest(t, "(1 . a)", cons(1, sym("a")))
-	printTest(t, "(quote . a)", cons(sym("quote"), sym("a")))
+	m := "TestPrint"
+	assertEquals(t, m, "abc", "abc", print(sym("abc")))
+	assertEquals(t, m, "123", "123", print(123))
+	assertEquals(t, m, "true", "true", print(true))
+	assertEquals(t, m, "()", "()", print(list()))
+	assertEquals(t, m, "(1 a)", "(1 a)", print(list(1, sym("a"))))
+	assertEquals(t, m, "'(1 a)", "'(1 a)", print(quote(list(1, sym("a")))))
+	assertEquals(t, m, "(1 . a)", "(1 . a)", print(cons(1, sym("a"))))
+	assertEquals(t, m, "(quote . a)", "(quote . a)", print(cons(sym("quote"), sym("a"))))
 }
 
 type Cons0Interface struct {
@@ -84,12 +79,13 @@ type Cons3Interface struct {
 }
 
 func TestSize(t *testing.T) {
-	assertEquals(t, "TestSize", "Sizeof(2)", 8, unsafe.Sizeof(2))
-	assertEquals(t, "TestSize", "Sizeof(\"abcd\")", 16, unsafe.Sizeof("abcd"))
-	assertEquals(t, "TestSize", "Sizeof(Cons0Interface{})", 0, unsafe.Sizeof(Cons0Interface{}))
-	assertEquals(t, "TestSize", "Sizeof(Cons2Interface{2, 3})", 32, unsafe.Sizeof(Cons2Interface{2, 3}))
-	assertEquals(t, "TestSize", "Sizeof(&Cons2Interface{2, 3})", 8, unsafe.Sizeof(&Cons2Interface{2, 3}))
-	assertEquals(t, "TestSize", "Sizeof(Cons2Interface{\"a\", \"b\"})", 32, unsafe.Sizeof(Cons2Interface{"a", "b"}))
-	assertEquals(t, "TestSize", "Sizeof(Cons2Interface{\"a\", Cons2Interface{\"b\", \"c\"}})", 32, unsafe.Sizeof(Cons2Interface{"a", Cons2Interface{"b", "c"}}))
-	assertEquals(t, "TestSize", "Sizeof(Cons3Interface{2, 3, 4})", 48, unsafe.Sizeof(Cons3Interface{2, 3, 4}))
+	m := "TestSize"
+	assertEquals(t, m, "Sizeof(2)", 8, unsafe.Sizeof(2))
+	assertEquals(t, m, "Sizeof(\"abcd\")", 16, unsafe.Sizeof("abcd"))
+	assertEquals(t, m, "Sizeof(Cons0Interface{})", 0, unsafe.Sizeof(Cons0Interface{}))
+	assertEquals(t, m, "Sizeof(Cons2Interface{2, 3})", 32, unsafe.Sizeof(Cons2Interface{2, 3}))
+	assertEquals(t, m, "Sizeof(&Cons2Interface{2, 3})", 8, unsafe.Sizeof(&Cons2Interface{2, 3}))
+	assertEquals(t, m, "Sizeof(Cons2Interface{\"a\", \"b\"})", 32, unsafe.Sizeof(Cons2Interface{"a", "b"}))
+	assertEquals(t, m, "Sizeof(Cons2Interface{\"a\", Cons2Interface{\"b\", \"c\"}})", 32, unsafe.Sizeof(Cons2Interface{"a", Cons2Interface{"b", "c"}}))
+	assertEquals(t, m, "Sizeof(Cons3Interface{2, 3, 4})", 48, unsafe.Sizeof(Cons3Interface{2, 3, 4}))
 }
